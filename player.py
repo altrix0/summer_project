3c1b6369d0b141e86bb88f853ec9988678c13247
altrix0 <anshasingh10@gmail.com>
2021-05-29 22:31:26 +0530

    Changed the buttons and added, alligned and named some frames

--- player.py
+++ player.py
@@ -4,11 +4,14 @@ from tkinter import filedialog
 import time
 from mutagen.mp3 import MP3
 import tkinter.ttk as ttk
+from tkinter import font
 
 root = Tk()
 root.title('MP3 Player')
 root.iconbitmap('C:/Users/ansha/Desktop/summer_project/gui/images/mp3.ico')
-root.geometry('500x400')
+root.geometry('550x458')
+root.configure(bg='grey')
+
 
 # Initialize pygame mixer
 pygame.mixer.init()
@@ -251,27 +254,37 @@ def volume(x):
 	# slider_label.config(text=int(current_volume * 100))
 
 # Create Master Frame
-master_frame = Frame(root)
+master_frame = LabelFrame(root, text=' ---Song List---------------------------------------------------------', border=0, font=('Arial', 11, 'italic') )
 master_frame.pack(pady=20)
+master_frame.configure(bg='white')
 
 # Create playlist box
-list_box = Listbox(master_frame, bg='black', fg='white', width=60, selectbackground="white", selectforeground="black")
+list_box = Listbox(master_frame, bg='#333333', fg='#00f7ff', width=60, selectbackground="white", selectforeground="black")
 list_box.grid(row=0, column=0)
 
 # Create player control buttons
-back_butn_img = PhotoImage(file='gui/images/back50.png')
-frwrd_butn_img = PhotoImage(file='gui/images/forward50.png')
-play_butn_img = PhotoImage(file='gui/images/play50.png')
-pause_butn_img = PhotoImage(file='gui/images/pause50.png')
-stop_butn_img = PhotoImage(file='gui/images/stop50.png')
+back_butn_img = PhotoImage(file='gui/images/backcustom50.png')
+frwrd_butn_img = PhotoImage(file='gui/images/nextcustom50.png')
+play_butn_img = PhotoImage(file='gui/images/playcustom50.png')
+pause_butn_img = PhotoImage(file='gui/images/pausecustom50.png')
+stop_butn_img = PhotoImage(file='gui/images/stopcustom50.png')
 
 # Create player control frame
-control_frame = Frame(master_frame)
+control_frame = LabelFrame(master_frame, text='Song Controls', font=('Arial', 10, 'italic'))
 control_frame.grid(row=1, column=0, pady=20)
+control_frame.configure(bg='white')
 
 # Create volume label frame
-volume_frame = LabelFrame(master_frame, text='Volume')
+volume_frame = LabelFrame(master_frame, text='Volume',
+                          font=('Arial', 10, 'italic'))
 volume_frame.grid(row=0, column=1, padx=30)
+volume_frame.configure(bg='white')
+
+# Create slider frame
+slider_frame = LabelFrame(master_frame, font=('Arial', 10, 'italic'))
+slider_frame.grid(row=2, column=0, padx=5, pady=10)
+slider_frame.configure(bg='white')
+# slider_frame.place(relx=0.5, rely=0.81, anchor='center')
 
 # Create player control buttons
 back_butn = Button(control_frame, image=back_butn_img, borderwidth=0,command=prev_song)
@@ -310,16 +323,16 @@ status_bar.pack(fill=X, side=BOTTOM, ipady=2)
 
 
 # Slider
-my_slider = ttk.Scale(master_frame, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=360)
-my_slider.grid(row=2, column=0, pady=10)
+my_slider = ttk.Scale(slider_frame, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=360)
+my_slider.pack(pady=10)
 
 # Create volume slider
 volume_slider = ttk.Scale(volume_frame, from_=0, to=1, orient=VERTICAL, value=1, command=volume, length=130)
 volume_slider.pack(pady=10)
 
 # Create temp. slider lable
-slider_label = Label(root, text='0')
-slider_label.pack(pady=10)
+# slider_label = Label(root, text='0')
+# slider_label.pack(pady=10, padx=10)
 
 
 root.mainloop()
